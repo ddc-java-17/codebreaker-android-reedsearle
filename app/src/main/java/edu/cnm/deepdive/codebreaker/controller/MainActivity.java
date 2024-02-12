f9d8d2a6dc2bfaff6649c5874ab8ae6554171e62
Reed Searle <csearle@cnm.edu>
2024-02-12 14:33:33 -0700

    Signout in progress

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import android.os.Bundle;
+import android.view.Menu;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.databinding.ActivityMainBinding;
+import edu.cnm.deepdive.codebreaker.viewmodel.LoginViewModel;
+
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+
+  private ActivityMainBinding binding;
+  private LoginViewModel loginViewModel;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+    setContentView(binding.getRoot());
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.main_options, menu);
+    return true;
+  }
+
+  @Override
+  public void onOptionsMenuClosed(Menu menu) {
+    super.onOptionsMenuClosed(menu);
+  }
+}
